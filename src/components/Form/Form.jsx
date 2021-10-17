6ef24b1abe51e7d3c31d47b22288ca013b55a3fb
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-10-17 12:54:41 +0200

    add contacts-actions

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,38 +1,32 @@
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contacts-actions';
 import s from './Form.module.css';
 
-export default function Form({ onSubmit }) {
+export default function Form() {
+  const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const nameId = uuidv4();
-  const telId = uuidv4();
+  const handleChange = e => {
+    const { name, value = '' } = e.target;
+    name === 'name' ? setName(value) : setNumber(value);
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(name, number);
+  const resetState = () => {
     setName('');
     setNumber('');
   };
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-      case 'number':
-        setNumber(value);
-        break;
-      default:
-        return;
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(addContact(name, number));
+    resetState();
   };
 
   return (
     <form className={s.form} onSubmit={handleSubmit}>
-      <label htmlFor={nameId} className={s.label}>
+      <label className={s.label}>
         Name
         <input
           className={s.input}
@@ -41,14 +35,13 @@ export default function Form({ onSubmit }) {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
-          id={nameId}
           onChange={handleChange}
           value={name}
           placeholder="Annie Copeland"
         />
       </label>
 
-      <label htmlFor={telId} className={s.label}>
+      <label className={s.label}>
         Number
         <input
           className={s.input}
@@ -57,7 +50,6 @@ export default function Form({ onSubmit }) {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
           required
-          id={telId}
           onChange={handleChange}
           value={number}
           placeholder="227-91-26"
