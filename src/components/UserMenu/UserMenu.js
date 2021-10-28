9a84df11e3af59dd747627f43ca637e92c34874e
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-10-28 22:19:52 +0200

    add appBar, Routers

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Navbar } from 'react-bootstrap';
+import { logOut } from '../../redux/auth/auth-operations';
+import authSelectors from '../../redux/auth/auth.selectors';
+
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUsername);
+
+  return (
+    <Navbar>
+      <p>Welcome, {name}</p>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Sign out
+      </button>
+    </Navbar>
+  );
+}
